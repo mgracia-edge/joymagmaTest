8b2cf1701231c3d3be0954ceba653d00862b361e
ffernandez <ffernandez@edge-americas.com>
2019-01-25 12:29:39 -0300

    Crear ingestor y scheduler de EPG desde FTP

--- api/playme.js
+++ api/playme.js
@@ -1,3 +1,9 @@
+const path = require("path");
+const fs = require('fs');
+const request = require("request");
+const Client = require('ftp');
+const parseString = require('xml2js').parseString;
+
 const pdc = require("../lib/provisoryDataConnection");
 
 exports.resourceList = [
@@ -6,8 +12,149 @@ exports.resourceList = [
         callback: ABMPlayme,
         method: "post",
         protected: true
+    },
+    {
+        path: "verify",
+        callback: LoginWOTT,
+        method: "post",
+        protected: false
     }
 ];
+
+/** Programme Update Service ***/
+
+function listProgramme() {
+    return new Promise((resolve, reject) => {
+        parseString(fs.readFileSync(path.join(__dirname, '../res/reportv.xml'), 'latin1'), function (err, result) {
+            resolve(result.tv.programme)
+        });
+    })
+
+
+}
+
+function downloadReportTV() {
+    return new Promise((resolve,reject)=>{
+
+        let ftp = new Client();
+
+        ftp.on('ready', () => {
+
+            ftp.list((err, list) => {
+                if (err) reject();
+
+                let lastFile = list[list.length - 1].name;
+
+                ftp.get(lastFile, function (err, stream) {
+                    if (err) reject();
+
+                    stream.once('close', function () {
+
+                        resolve();
+
+                        ftp.end();
+
+                    });
+
+                    stream.pipe(fs.createWriteStream(path.join(__dirname, '../res/reportv.xml')));
+                });
+            });
+        });
+
+        ftp.connect({
+            host: 'ftp.filestv.com.ar',
+            user: 'NexTV',
+            password: 'nextvepg123'
+        });
+    });
+}
+
+let programmeUpdateService = setTimeout(() => {
+
+    function parseDate(string) {
+        return new Date(
+            string.substring(0, 4) + "/" +
+            string.substring(4, 6) + "/" +
+            string.substring(6, 8)
+        ).setHours(string.substring(8, 10),string.substring(10, 12),0);
+    }
+
+    downloadReportTV()
+        .then(listProgramme)
+        .then((list) => {
+        let db = pdc.db;
+
+        if (db) {
+
+            db.Programme.deleteMany().then(()=>{
+                let arr = [];
+
+                console.log("Registros Eliminados");
+                console.log("Numeros de Programas", list.length);
+
+                list.forEach((item) => {
+
+                    let json = {
+                        "start": parseDate(item["$"].start),
+                        "stop": parseDate(item["$"].stop),
+                        "title": item.title[0]["_"],
+                        "description": item.desc[0]["_"],
+                        "credits": item.credits ? item.credits[0] : '',
+                        "category": parseInt(item.category[0]["_"]),
+                        "subCategory": parseInt(item["sub-category"][0]["_"]),
+                        "parentalCode": parseInt(item["$"]['parental-code']),
+                        "channelEPGId": parseInt(item["$"]['channel'])
+                    };
+
+                    arr.push(json);
+
+                });
+
+                db.Programme.insertMany(arr, (err) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+
+                    }
+
+                });
+            });
+
+
+
+        } else {
+            console.log("error")
+        }
+    });
+
+}, 1000 * 3600 * 24);
+
+/** API ***/
+
+function LoginWOTT(req, res) {
+
+    let options = {
+        url: 'https://ws.wiltel.com.ar/WS/CRM.asmx/LoginWOTT',
+        agentOptions: {
+            pfx: fs.readFileSync(path.join(__dirname, './EPA.pfx')),
+            passphrase: 'Wiltel19',
+            securityOptions: 'SSL_OP_NO_SSLv3'
+        },
+        formData: {
+            Usuario: "epa",
+            Password: "wil2019tel"
+        }
+    };
+
+    let result = request.post(options, (err, httpResponse, body) => {
+        if (err) res.send(err);
+        res.status(200).send(body);
+    });
+
+    //console.log(result)
+
+
+}
 
 function ABMPlayme(req, res) {
 
@@ -157,3 +304,4 @@ function ABMPlayme(req, res) {
 
 
 }
+
