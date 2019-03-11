bfa8b67f9784c8da7383349fe2892a66bd09deeb
ffernandez <ffernandez@edge-americas.com>
2019-03-11 18:03:56 -0300

    Se agregan vistas - Account Serrings - Account Access - User Details

--- api/nx/account.js
+++ api/nx/account.js
@@ -1,11 +1,35 @@
 const api = require('../support');
 const codes = require('../codes');
 const dc = require('../../lib/dataConnection');
+const cloudinary = require('cloudinary');
 
 exports.resourceList = [
     {
         path: "get",
         callback: _get,
+        method: "post",
+        protected: true
+    },{
+        path: "create",
+        callback: _create,
+        method: "post",
+        protected: true
+    },
+    {
+        path: "read",
+        callback: _read,
+        method: "post",
+        protected: true
+    },
+    {
+        path: "update",
+        callback: _update,
+        method: "post",
+        protected: true
+    },
+    {
+        path: "delete",
+        callback: _delete,
         method: "post",
         protected: true
     }];
@@ -29,6 +53,129 @@ function _get(req, res) {
         })
 
     } else {
+        res.status(codes.error.database.DISCONNECTED.httpCode)
+            .send(new api.Error(codes.error.database.DISCONNECTED));
+    }
+}
+
+/* CRUD */
+
+/* TODO: */
+function _create(req, res) {
+    let db = dc.db;
+
+    if (db) {
+
+        if (!req.user.permissions.includes(codes.users_permissions.USER_ADMIN)) {
+
+            res.status(codes.error.userRights.PERMISSION_DENIED.httpCode)
+                .send(new api.Error(codes.error.userRights.PERMISSION_DENIED));
+
+            return;
+        }
+
+    } else {
+
+        res.status(codes.error.database.DISCONNECTED.httpCode)
+            .send(new api.Error(codes.error.database.DISCONNECTED));
+    }
+}
+/* TODO: */
+function _read(req, res) {
+    let db = dc.db;
+
+    if (db) {
+
+        if (!req.user.permissions.includes(codes.users_permissions.USER_ADMIN)) {
+
+            res.status(codes.error.userRights.PERMISSION_DENIED.httpCode)
+                .send(new api.Error(codes.error.userRights.PERMISSION_DENIED));
+
+            return;
+        }
+
+    } else {
+
+        res.status(codes.error.database.DISCONNECTED.httpCode)
+            .send(new api.Error(codes.error.database.DISCONNECTED));
+    }
+}
+
+function _update(req, res) {
+    let db = dc.db;
+
+    if (db) {
+
+        if (!req.user.permissions.includes(codes.users_permissions.USER_ADMIN)) {
+
+            res.status(codes.error.userRights.PERMISSION_DENIED.httpCode)
+                .send(new api.Error(codes.error.userRights.PERMISSION_DENIED));
+
+            return;
+        }
+
+        const {id, data} = req.body;
+
+        const {name, description, logo} = data;
+
+        let query = {
+            find: {
+                _id: id
+            },
+            update: {
+                $set: {}
+            }
+        };
+
+        if (typeof name !== 'undefined') query.update.$set["name"] = name;
+        if (typeof description !== 'undefined') query.update.$set["description"] = description;
+
+        if(typeof logo !== 'undefined' && logo.update === true){
+            cloudinary.uploader.upload(logo.url, (result) => {
+                query.update.$set["logo.url"] = result.url;
+                _update();
+            })
+        }else{
+            _update();
+        }
+
+        function _update(){
+            db.Account.updateOne(query.find, query.update, (error, products) => {
+                if (error) {
+                    res.status(codes.error.operation.OPERATION_HAS_FAILED.httpCode)
+                        .send(new api.Error(codes.error.operation.OPERATION_HAS_FAILED));
+                } else {
+                    res.status(200).send(new api.Success(products));
+                }
+
+            });
+        }
+
+
+
+    } else {
+
+        res.status(codes.error.operation.DISCONNECTED.httpCode)
+            .send(new api.Error(codes.error.database.DISCONNECTED));
+    }
+}
+/* TODO: */
+function _delete(req, res) {
+
+    let db = dc.db;
+
+    if (db) {
+
+        if (!req.user.permissions.includes(codes.users_permissions.USER_ADMIN)) {
+
+            res.status(codes.error.userRights.PERMISSION_DENIED.httpCode)
+                .send(new api.Error(codes.error.userRights.PERMISSION_DENIED));
+
+            return;
+        }
+
+    } else {
+
         res.status(codes.error.database.DISCONNECTED.httpCode)
             .send(new api.Error(codes.error.database.DISCONNECTED));
     }
--- public/javascript/controller_s_account_settings_user.js
+++ public/javascript/controller_s_account_settings_user.js
@@ -13,213 +13,221 @@
 (function () {
     angular.module('NxStudio')
         .controller("sAccountSettingsUserCtrl",
-            ['$scope', '$NxApi', '$mdToast', '$location', '$routeParams','$q','$mdDialog',
-                function ($scope, $NxApi, $mdToast, $location, $routeParams,$q,$mdDialog) {
+            ['$scope', '$NxApi', '$mdToast', '$location', '$routeParams', '$q', '$mdDialog',
+                function ($scope, $NxApi, $mdToast, $location, $routeParams, $q, $mdDialog) {
 
-            const users_permissions = {
-                USER_ADMIN: "user.admin",
-                PRODUCTS_WRITE: "products.write",
-                PRODUCTS_READ: "products.read",
-                CHANNELS_WRITE: "channels.write",
-                CHANNELS_READ: "channels.read",
-                STATS_ACCESS: "stats.access",
-                SUBSCRIBERS_READ: "subscribers.read",
-            };
-
-            $scope.user = {};
-            $scope.accountName = '';
-            $scope.users_permissions = [
-                {
-                    value:users_permissions.CHANNELS_READ,
-                    state:false
-                },
-                {
-                    value:users_permissions.CHANNELS_WRITE,
-                    state:false
-                },
-                {
-                    value:users_permissions.PRODUCTS_READ,
-                    state:false
-                },
-                {
-                    value:users_permissions.PRODUCTS_WRITE,
-                    state:false
-                },
-                {
-                    value:users_permissions.STATS_ACCESS,
-                    state:false
-                },
-                {
-                    value:users_permissions.SUBSCRIBERS_READ,
-                    state:false
-                },
-                {
-                    value:users_permissions.USER_ADMIN,
-                    state:false
-                }
-            ];
-            $scope.loading = false;
-
-            $scope.updateUser = updateUser;
-            $scope.getUrlProfilePhoto = getUrlProfilePhoto;
-            $scope.dialog_changePassword = dialog_changePassword;
-            $scope.dialog_alert = dialog_alert;
-
-            function init() {
-                let user_id = $routeParams.user_id;
-
-                if (user_id) {
-                    $NxApi.users.read({_id: user_id}).then(({users}) => {
-                        $scope.user = users[0];
-                        for(let permission of $scope.users_permissions){
-                            if($scope.user.permissions.includes(permission.value)){
-                                permission.state = true;
-                            }
-                        }
-                    });
-
-                }
-
-            }
-
-            function toast(msg) {
-                $mdToast.show(
-                    $mdToast.simple()
-                        .textContent(msg)
-                        .hideDelay(5000))
-                    .then(function () {
-                    }).catch(function () {
-                });
-            }
-
-            function checkForm() {
-
-                let {firstName, lastName} = $scope.user;
-
-                if (firstName !== '' && lastName !== '') {
-                    return true
-                }
-
-                toast("The fields cannot be empty");
-
-                return false
-            }
-
-            function updateUser() {
-
-                if (checkForm()) {
-
-                    $scope.loading = true;
-
-                    let data = {
-                        _id: $scope.user._id,
-                        firstName: $scope.user.firstName,
-                        lastName: $scope.user.lastName,
-                        permissions: _getPermissions()
+                    const users_permissions = {
+                        USER_ADMIN: "user.admin",
+                        PRODUCTS_WRITE: "products.write",
+                        PRODUCTS_READ: "products.read",
+                        CHANNELS_WRITE: "channels.write",
+                        CHANNELS_READ: "channels.read",
+                        STATS_ACCESS: "stats.access",
+                        SUBSCRIBERS_READ: "subscribers.read",
                     };
 
-                    $NxApi.users
-                        .update(data)
-                        .then(() => {
-                            toast('The user was update');
-                            $scope.loading = false;
+                    $scope.user = {};
+                    $scope.accountName = '';
+                    $scope.users_permissions = [
+                        {
+                            value: users_permissions.CHANNELS_READ,
+                            state: false
+                        },
+                        {
+                            value: users_permissions.CHANNELS_WRITE,
+                            state: false
+                        },
+                        {
+                            value: users_permissions.PRODUCTS_READ,
+                            state: false
+                        },
+                        {
+                            value: users_permissions.PRODUCTS_WRITE,
+                            state: false
+                        },
+                        {
+                            value: users_permissions.STATS_ACCESS,
+                            state: false
+                        },
+                        {
+                            value: users_permissions.SUBSCRIBERS_READ,
+                            state: false
+                        },
+                        {
+                            value: users_permissions.USER_ADMIN,
+                            state: false
+                        }
+                    ];
+                    $scope.loading = false;
 
-                        })
-                        .catch((error) => {
-                            console.log(error);
-                            $scope.loading = false;
-                        })
-                }
+                    $scope.updateUser = updateUser;
+                    $scope.uploadImage = uploadImage;
+                    $scope.getUrlProfilePhoto = getUrlProfilePhoto;
+                    $scope.dialog_changePassword = dialog_changePassword;
+                    $scope.dialog_alert = dialog_alert;
+                    $scope.logAsUser = logAsUser;
 
-            }
+                    function init() {
+                        let user_id = $routeParams.user_id;
 
-            function getUrlProfilePhoto(user){
-                if(user.photo && user.photo.url){
-                    return user.photo.url
-                }
-
-                return '/res/drawable/ph_user.jpg'
-            }
-
-            function _getPermissions(){
-                let user_permissions = [];
-                for(let permission of $scope.users_permissions){
-                    if(permission.state) user_permissions.push(permission.value);
-                }
-                return user_permissions;
-            }
-
-            function dialog_changePassword() {
-
-                let title = 'Reset Password';
-                let templateUrl = "/res/layout/dialog_change_password.html";
-                let userId = $scope.user._id;
-
-                return $q((resolve, reject) => {
-
-                    let dialog = {
-                        templateUrl: templateUrl,
-                        parent: angular.element(document.body),
-                        escapeToClose: true,
-                        clickOutsideToClose: true,
-                        controller: dialogController
-                    };
-
-                    $mdDialog.show(dialog).then(resolve, reject);
-
-                    function dialogController($scope, $mdDialog) {
-
-                        $scope.title = title;
-                        $scope.userId = userId;
-                        $scope.newPassword = '';
-                        $scope.newPasswordVerify = '';
-                        $scope.loading = false;
-
-                        $scope.cancel = cancel;
-                        $scope.save = save;
-                        $scope.verifyPassword = verifyPassword;
-
-                        function init() {
+                        if (user_id) {
+                            $NxApi.users.read({_id: user_id}).then(({users}) => {
+                                $scope.user = users[0];
+                                for (let permission of $scope.users_permissions) {
+                                    if ($scope.user.permissions.includes(permission.value)) {
+                                        permission.state = true;
+                                    }
+                                }
+                            });
 
                         }
 
-                        function cancel() {
-                            $mdDialog.cancel();
+                    }
+
+                    function toast(msg) {
+                        $mdToast.show(
+                            $mdToast.simple()
+                                .textContent(msg)
+                                .hideDelay(5000))
+                            .then(function () {
+                            }).catch(function () {
+                        });
+                    }
+
+                    function checkForm() {
+
+                        let {firstName, lastName, email} = $scope.user;
+
+                        if (firstName !== '' && lastName !== '' && email !== '') {
+                            return true
                         }
 
-                        function verifyPassword() {
+                        toast("The fields cannot be empty");
 
-                            return ($scope.newPassword === '' && $scope.newPasswordVerify === '') || $scope.newPassword !== $scope.newPasswordVerify
-                        }
+                        return false
+                    }
 
-                        function save() {
+                    function updateUser() {
+
+                        if (checkForm()) {
+
                             $scope.loading = true;
+
+                            let data = {
+                                _id: $scope.user._id,
+                                firstName: $scope.user.firstName,
+                                lastName: $scope.user.lastName,
+                                permissions: _getPermissions(),
+                                email: $scope.user.email
+                            };
+
+                            if($scope.user.photo){
+                                data["photo"] = $scope.user.photo;
+                            }
+
                             $NxApi.users
-                                .update({
-                                    _id: userId,
-                                    password: $scope.newPassword
-                                })
+                                .update(data)
                                 .then(() => {
-                                    toast('The password was update');
+                                    toast('The user was update');
                                     $scope.loading = false;
-                                    $mdDialog.hide();
+                                    if($scope.user.photo) $scope.user.photo.update = false;
 
                                 })
                                 .catch((error) => {
                                     console.log(error);
                                     $scope.loading = false;
                                 })
-
                         }
 
-                        init();
                     }
 
-                });
+                    function getUrlProfilePhoto(user) {
+                        if (user.photo && user.photo.url) {
+                            return user.photo.url
+                        }
 
-            }
+                        return '/res/drawable/ph_user.jpg'
+                    }
 
-            function dialog_alert() {
+                    function _getPermissions() {
+                        let user_permissions = [];
+                        for (let permission of $scope.users_permissions) {
+                            if (permission.state) user_permissions.push(permission.value);
+                        }
+                        return user_permissions;
+                    }
+
+                    function dialog_changePassword() {
+
+                        let title = 'Reset Password';
+                        let templateUrl = "/res/layout/dialog_change_password.html";
+                        let userId = $scope.user._id;
+
+                        return $q((resolve, reject) => {
+
+                            let dialog = {
+                                templateUrl: templateUrl,
+                                parent: angular.element(document.body),
+                                escapeToClose: true,
+                                clickOutsideToClose: true,
+                                controller: dialogController
+                            };
+
+                            $mdDialog.show(dialog).then(resolve, reject);
+
+                            function dialogController($scope, $mdDialog) {
+
+                                $scope.title = title;
+                                $scope.userId = userId;
+                                $scope.newPassword = '';
+                                $scope.newPasswordVerify = '';
+                                $scope.loading = false;
+
+                                $scope.cancel = cancel;
+                                $scope.save = save;
+                                $scope.verifyPassword = verifyPassword;
+
+                                function init() {
+
+                                }
+
+                                function cancel() {
+                                    $mdDialog.cancel();
+                                }
+
+                                function verifyPassword() {
+
+                                    return ($scope.newPassword === '' && $scope.newPasswordVerify === '') || $scope.newPassword !== $scope.newPasswordVerify
+                                }
+
+                                function save() {
+                                    $scope.loading = true;
+                                    $NxApi.users
+                                        .update({
+                                            _id: userId,
+                                            password: $scope.newPassword
+                                        })
+                                        .then(() => {
+                                            toast('The password was update');
+                                            $scope.loading = false;
+                                            $mdDialog.hide();
+
+                                        })
+                                        .catch((error) => {
+                                            console.log(error);
+                                            $scope.loading = false;
+                                        })
+
+                                }
+
+                                init();
+                            }
+
+                        });
+
+                    }
+
+                    function dialog_alert() {
 
                         let title = 'Remove user';
                         let description = 'You are sure to delete the current user?';
@@ -259,13 +267,14 @@
                                 function ok() {
                                     $scope.loading = true;
                                     $NxApi.users
-                                        .remove({
+                                        .delete({
                                             _id: userId
                                         })
                                         .then(() => {
-                                            toast('The password was update');
+                                            toast('user removed');
                                             $scope.loading = false;
                                             $mdDialog.hide();
+                                            $location.path('/s/account/settings/users');
 
                                         })
                                         .catch((error) => {
@@ -282,9 +291,50 @@
 
                     }
 
-            init();
+                    function _getImage() {
+                        return $q((resolve, reject) => {
+                            let file = document.createElement('input');
+                            let maxSize = 500; //kb
+                            file.type = 'file';
+                            file.click();
 
-            $NxApi.setAfterLogin(init);
+                            file.addEventListener('change', function () {
 
-        }]);
+                                if (file.files[0].size / 1000 > maxSize) {
+                                    reject(`The image can not be larger than ${maxSize}kb`)
+                                }
+
+                                let reader = new FileReader();
+
+                                reader.onloadend = function () {
+                                    resolve(reader.result)
+                                };
+
+                                reader.readAsDataURL(file.files[0]);
+                            })
+                        })
+                    }
+
+                    function uploadImage() {
+                        _getImage().then((img) => {
+                            $scope.user.photo = {
+                                update:true,
+                                url:img
+                            };
+                        }).catch((error) => {
+                            toast(error);
+                        })
+                    }
+
+                    function logAsUser(user){
+                        $NxApi.loginAsUser(user.email,user.authentication.password).then(()=>{
+                            window.location = "/";
+                        })
+                    }
+
+                    init();
+
+                    $NxApi.setAfterLogin(init);
+
+                }]);
 })();
