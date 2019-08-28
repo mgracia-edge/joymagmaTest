7689db16339ccad1b156fdd24d21543fa9b92839
Pablo D'Ambrosio <pdambrosio@edge-americas.com>
2019-08-28 12:22:37 +0200

    adding section menu and controller for stats

--- public/javascript/controller_s_ott_statistics.js
+++ public/javascript/controller_s_ott_statistics.js
@@ -14,7 +14,69 @@
     angular.module('NxStudio')
         .controller("sOttStatisticsCtrl", ['$scope', '$NxApi', '$mdToast', '$location', function ($scope, $NxApi, $mdToast, $location) {
 
+
+            $scope.sectionName = "Choose a section";
+
+            $scope.sections = [
+                {
+                    name: "Subscribers",
+                    icon:"/res/drawable/ic_users.svg",
+                    controller: SubscribersCtrl
+                }, {
+                    name: "Viewing",
+                    icon:"/res/drawable/ic_viewing.svg",
+                    controller: ViewingCtrl
+                }, {
+                    name: "Devices",
+                    icon:"/res/drawable/ic_devices.svg",
+                    controller: DevicesCtrl
+                }
+            ];
+
+            $scope.startSection = startSection;
+
             function init() {
+                startSection($scope.sections[0]);
+            }
+
+            let cleanUp = (_) => {
+            };
+
+            function startSection(section) {
+
+                if (typeof section.controller === "function") {
+                    cleanUp();
+                    $scope.currentSection = section;
+                    section.controller();
+                }
+
+            }
+
+
+            function SubscribersCtrl() {
+                cleanUp = (_) => {
+                    // Setup a new Clean Up Function in case other section is choose.
+
+
+                }
+
+            }
+
+            function ViewingCtrl() {
+                cleanUp = (_) => {
+                    // Setup a new Clean Up Function in case other section is choose.
+
+
+                }
+
+            }
+
+            function DevicesCtrl() {
+                cleanUp = (_) => {
+                    // Setup a new Clean Up Function in case other section is choose.
+
+
+                }
 
             }
 
